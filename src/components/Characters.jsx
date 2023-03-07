6735b4b142563aa6a19965f1844dbaddd727a4ff
pablosimkievich <pablosimkievich@gmail.com>
2023-03-07 18:24:00 -0300

    commit inicial

---
+++ src/components/Characters.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Characters = ( {characters = []} ) => {
+    return (
+
+        <div className='row'>
+                {characters.map((item, index) => (
+                    <div key={index} className='col-3 mb-4 '>
+                        <div className='card-header border-5 border-info'  style={{minHeight: 450}}>
+                            <img className='card-img-top rounded' src={item.image} alt='' ></img>
+                            <div className='card-body'>
+                                <h5 className='card-title'>{item.name}</h5>
+                                <hr />
+                                <p className='card-subtitle mb-2 text-muted'>Specie: {item.species}</p>
+                                <p className='card-subtitle mb-2 text-muted'>Location: {item.location.name}</p>
+                                
+                            </div>
+                        </div>
+                    </div>                   
+                ))}
+        </div>
+    )
+};
+
+export default Characters;
